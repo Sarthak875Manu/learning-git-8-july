bc1c16a9b2b3d66eb1e3b55614b867c5498bde3b
unknown <sarthaksharma875@gmail.com>
2025-07-08 08:42:40 +0530

    conflict-1

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-first"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "second-rg"
+  location = "eastus2"
+}
